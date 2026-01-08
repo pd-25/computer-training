405a7338f66d30f74f566244f4ad77bed3eaa9cb
artic0909 <alfag327@gmail.com>
2026-01-08 20:52:01 +0530

    sss

--- resources/views/frontend/partials/menu.blade.php
+++ resources/views/frontend/partials/menu.blade.php
@@ -2,15 +2,7 @@
     <li class="nav-item {{ Route::currentRouteName() == 'frontend.index' ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('frontend.index') }}">Home</a>
     </li>
-    <li class="nav-item {{ Route::currentRouteName() == 'frontend.about' ? 'active' : '@@about' }}">
-        <a class="nav-link" href="{{ route('frontend.about') }}">About</a>
-    </li>
-    <li class="nav-item @@courses">
-        <a class="nav-link" href="#">Director message</a>
-    </li>
-    <li class="nav-item {{ Route::currentRouteName() == 'frontend.gallery' ? 'active' : '@@gallery' }}">
-        <a class="nav-link" href="{{ route('frontend.gallery') }}">Gallery</a>
-    </li>
+    
     <li class="nav-item dropdown view">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
             aria-haspopup="true" aria-expanded="false">
@@ -34,12 +26,23 @@
 
         </ul>
     </li>
+
+    <li class="nav-item {{ Route::currentRouteName() == 'frontend.verification' ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('frontend.verification') }}">Student Zone</a>
+    </li>
+
+    <li class="nav-item {{ Route::currentRouteName() == 'frontend.franchise' ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('frontend.franchise') }}">Get Franchise</a>
+    </li>
+
     <li class="nav-item dropdown view">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
             aria-haspopup="true" aria-expanded="false">
             Others
         </a>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item {{ Route::currentRouteName() == 'frontend.gallery' ? 'active' : '' }}" href="{{ route('frontend.gallery') }}">Gallery</a></li>
+            <li><a class="dropdown-item" href="#">Director message</a></li>
             <li><a class="dropdown-item {{ Route::currentRouteName() == 'frontend.mission' ? 'active' : '' }}" href="{{ route('frontend.mission') }}">Mission</a></li>
             <li><a class="dropdown-item {{ Route::currentRouteName() == 'frontend.vision' ? 'active' : '' }}" href="{{ route('frontend.vision') }}">vision</a></li>
             <li><a class="dropdown-item {{ Route::currentRouteName() == 'frontend.paynow' ? 'active' : '' }}" href="{{ route('frontend.paynow') }}">Pay Now</a></li>
@@ -49,10 +52,9 @@
             <li><a class="dropdown-item {{ Route::currentRouteName() == 'frontend.franchise-mode' ? 'active' : '' }}" href="{{ route('frontend.franchise-mode') }}">Franchise model</a></li>
             <li><a class="dropdown-item {{ Route::currentRouteName() == 'frontend.wallet' ? 'active' : '' }}" href="{{ route('frontend.wallet') }}">Wallet</a></li>
             <li><a class="dropdown-item {{ Route::currentRouteName() == 'frontend.verification' ? 'active' : '' }}" href="{{ route('frontend.verification') }}">Certificate verification</a></li>
-            <li><a class="dropdown-item {{ Route::currentRouteName() == 'frontend.student-zone' ? 'active' : '' }}" href="{{ route('frontend.student-zone') }}">Student Zone</a></li>
-            <li><a class="dropdown-item {{ Route::currentRouteName() == 'frontend.franchise' ? 'active' : '' }}" href="{{ route('frontend.franchise') }}">Get Franchise</a></li>
         </ul>
     </li>
+
     <li class="nav-item dropdown view">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
             aria-haspopup="true" aria-expanded="false">
@@ -63,6 +65,11 @@
             <li><a class="dropdown-item" href="{{ route('frontend.franchise-login') }}">Franchise Login</a></li>
         </ul>
     </li>
+
+    <li class="nav-item {{ Route::currentRouteName() == 'frontend.about' ? 'active' : '@@about' }}">
+        <a class="nav-link" href="{{ route('frontend.about') }}">About</a>
+    </li>
+
     <li class="nav-item {{ Route::currentRouteName() == 'frontend.contact' ? 'active' : '@@contact' }}">
         <a class="nav-link" href="{{ route('frontend.contact') }}">CONTACT</a>
     </li>
